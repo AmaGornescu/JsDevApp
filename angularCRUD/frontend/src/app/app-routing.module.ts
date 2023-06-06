dbcd89500196719bee2b63cc1f4db8c145dbfb23
Frontend JavaScript <frontendjavascript@C3-08.local>
2023-06-06 20:19:53 +0300

    angularCRUD

--- angularCRUD/frontend/src/app/app-routing.module.ts
+++ angularCRUD/frontend/src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
+import { ProfilComponent } from './profil/profil.component';
 
 const routes: Routes = [
   { path: 'prima-pagina', component: HomeComponent },
   { path: 'myadmin', component: AdminComponent },
+
+  { 
+    path: 'profil/:id', 
+    component:ProfilComponent,
+    // data: {
+    //   title: 'Profil user',
+    //   description: 'Profil user info'
+    // } 
+  },
   { path: '**', component: HomeComponent },  // Wildcard route for a 404 page
 ];
 
